ccb16dd71f4fb0001ae1a7e4e16330f6a2c84d98
Pramod Patil <pramod.patil5576@gmail.com>
2026-04-12 14:29:48 +0530

    TS1

---
+++ Chapter_15_TS/153_Function_void.ts
@@ -0,0 +1,11 @@
+function greet(name: string): void {
+    console.log("Hello " + name);
+}
+
+greet("John");
+
+function greet1(name) {
+    return console.log("Welcome " + name);
+}
+
+greet1("John");
